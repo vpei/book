36966aa1afb051834012ceedcb0559cd6d49e54c
vpei <vpei@live.com>
2024-01-05 00:27:55 +0800

    Initial commit

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # cd / && rm -rf /bookbot && git clone https://gitclone.com/github.com/vpei/book.git bookbot && rm -rf /bookbot/.git
 # mkdir /root/book/ && mv /book/* /root/book/ && rm -rf /book
 
-cd / && rm -rf /bookbot && wget https://ghproxy.com/https://raw.githubusercontent.com/vpei/book/main/book.zip -O bookbot.zip && unzip -d /bookbot/ bookbot.zip && rm -rf /bookbot.zip
+cd / && rm -rf /bookbot && wget https://raw.githubusercontent.com/vpei/book/main/book.zip -O bookbot.zip && unzip -d /bookbot/ bookbot.zip && rm -rf /bookbot.zip
 
 # nohup aria2c --conf-path=/root/.aria2/aria2.conf --rpc-listen-port=8080 --rpc-secret=$Aria2_secret &
 
